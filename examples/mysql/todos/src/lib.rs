759327d2318eb9bfe4a078070c25fcc9763a703c
Aditya <aditya.salunkh919@gmail.com>
2025-10-02 19:14:04 +0530

    feat: add mysql component
    Signed-off-by: Aditya <aditya.salunkh919@gmail.com>

---
+++ examples/mysql/todos/src/lib.rs
@@ -0,0 +1,117 @@
+use clap::{Parser, Subcommand};
+use futures::SinkExt as _;
+use sqlx::mysql::MySqlPool;
+use std::env;
+
+#[derive(Parser)]
+struct Args {
+    #[command(subcommand)]
+    cmd: Option<Command>,
+}
+
+#[derive(Subcommand)]
+enum Command {
+    Add { description: String },
+    Done { id: i64 },
+}
+
+async fn run() -> anyhow::Result<()> {
+    let args = Args::parse_from(wasi::cli::environment::get_arguments());
+    let pool = MySqlPool::connect(&env::var("DATABASE_URL")?).await?;
+
+    match args.cmd {
+        Some(Command::Add { description }) => {
+            eprintln!("Adding new todo with description '{description}'");
+            let todo_id = add_todo(&pool, description).await?;
+            eprintln!("Added new todo with id {todo_id}");
+        }
+        Some(Command::Done { id }) => {
+            eprintln!("Marking todo {id} as done");
+            if complete_todo(&pool, id).await? {
+                eprintln!("Todo {id} is marked as done");
+            } else {
+                eprintln!("Invalid id {id}");
+            }
+        }
+        None => {
+            eprintln!("Printing list of all todos");
+            list_todos(&pool).await?;
+        }
+    }
+
+    Ok(())
+}
+
+async fn add_todo(pool: &MySqlPool, description: String) -> anyhow::Result<i64> {
+    let result = sqlx::query!(
+        r#"
+INSERT INTO todos ( description )
+VALUES (?)
+        "#,
+        description
+    )
+    .execute(pool)
+    .await?;
+
+    Ok(result.last_insert_id() as i64)
+}
+
+async fn complete_todo(pool: &MySqlPool, id: i64) -> anyhow::Result<bool> {
+    let rows_affected = sqlx::query!(
+        r#"
+UPDATE todos
+SET done = TRUE
+WHERE id = ?
+        "#,
+        id
+    )
+    .execute(pool)
+    .await?
+    .rows_affected();
+
+    Ok(rows_affected > 0)
+}
+
+async fn list_todos(pool: &MySqlPool) -> anyhow::Result<()> {
+    let recs = sqlx::query!(
+        r#"
+SELECT id, description, done
+FROM todos
+ORDER BY id
+        "#
+    )
+    .fetch_all(pool)
+    .await?;
+
+    for rec in recs {
+        eprintln!(
+            "- [{}] {}: {}",
+            if rec.done != 0 { "x" } else { " " },
+            rec.id,
+            &rec.description,
+        );
+    }
+
+    Ok(())
+}
+
+struct Component;
+
+wasi::cli::command::export!(Component);
+
+impl wasi::exports::cli::run::Guest for Component {
+    async fn run() -> Result<(), ()> {
+        tokio::task::LocalSet::new()
+            .run_until(async {
+                if let Err(err) = run().await {
+                    let (mut tx, rx) = wasi::wit_stream::new();
+                    wasi::cli::stderr::set_stderr(rx);
+                    tx.send(format!("{err:#}\n").into()).await.or(Err(()))?;
+                    Err(())
+                } else {
+                    Ok(())
+                }
+            })
+            .await
+    }
+}
